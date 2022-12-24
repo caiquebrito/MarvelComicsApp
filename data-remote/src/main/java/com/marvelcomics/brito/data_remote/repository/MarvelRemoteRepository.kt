45c5a581f2f5276cb39fa1419a492fdb415aa831
Caique Brito <caique.brito.ads@gmail.com>
2022-12-23 23:28:26 -0300

    added new method to data remote and applied flows

--- data-remote/src/main/java/com/marvelcomics/brito/data_remote/repository/MarvelRemoteRepository.kt
+++ data-remote/src/main/java/com/marvelcomics/brito/data_remote/repository/MarvelRemoteRepository.kt
@@ -3,9 +3,9 @@ package com.marvelcomics.brito.data_remote.repository
 import com.marvelcomics.brito.data.remote.MarvelRemoteDataSource
 import com.marvelcomics.brito.data_remote.api.MarvelAPI
 import com.marvelcomics.brito.data_remote.datasource.response.fromResponseToEntity
-import com.marvelcomics.brito.data_remote.exception.MarvelApiException
 import com.marvelcomics.brito.data_remote.getBodyOrThrow
 import com.marvelcomics.brito.data_remote.handleApi
+import com.marvelcomics.brito.data_remote.throwIfNull
 import com.marvelcomics.brito.entity.CharacterEntity
 import com.marvelcomics.brito.entity.ComicEntity
 import com.marvelcomics.brito.entity.SeriesEntity
@@ -33,11 +33,4 @@ class MarvelRemoteRepository(
             api.series(characterId).getBodyOrThrow().fromResponseToEntity().throwIfNull()
         )
     }
-}
-
-@Throws(MarvelApiException::class)
-inline fun <reified T> T?.throwIfNull(): T {
-    this?.let {
-        return it
-    } ?: throw MarvelApiException("Error Mapping ${T::class.java.canonicalName}")
 }
--- data/src/main/java/com/marvelcomics/brito/data/repo/MarvelRepo.kt
+++ data/src/main/java/com/marvelcomics/brito/data/repo/MarvelRepo.kt
@@ -21,7 +21,7 @@ class MarvelRepo(
         return remote.getComicsById(characterId)
     }
 
-    override suspend fun getSeries(characterId: Int): Flow<List<SeriesEntity>> {
+    override fun getSeries(characterId: Int): Flow<List<SeriesEntity>> {
         return remote.getSeriesById(characterId)
     }
 
