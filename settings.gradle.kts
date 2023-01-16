4dbce7b740af1cc0c77939cc97c30c448058815f
Caique Brito <caique.brito.ads@gmail.com>
2023-01-15 23:59:08 -0300

    converted to gradle kts and catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,36 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven { setUrl("https://jitpack.io") }
     }
 }
 
+fun readProperties(propertiesFile: File) = java.util.Properties().apply {
+    try {
+        propertiesFile.inputStream().use { fis ->
+            load(fis)
+        }
+    } catch (ignored: Exception) {
+    }
+}
+
+val repo = readProperties(File("./build-system/repo.properties"))
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+        maven {
+            url = uri("https://repos.cora.tools/repository/maven-proxy")
+            credentials {
+                username = System.getenv("NEXUS_USER") ?: repo["USERNAME"] as String?
+                password = System.getenv("NEXUS_PASS") ?: repo["TOKEN"] as String?
+            }
+        }
+        maven { url = uri("https://jitpack.io") }
+        jcenter()
+    }
     versionCatalogs {
         create("libs") {
             from(files("./gradle/versions.toml"))
@@ -15,4 +41,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "MarvelComicsApp"
-include(":app",":domain",":presentation",":data",":data-remote",":data-local",":di",":entity",":marvel")
+include(":app", ":domain", ":presentation", ":data", ":data-remote", ":data-local", ":di", ":entity", ":marvel")
