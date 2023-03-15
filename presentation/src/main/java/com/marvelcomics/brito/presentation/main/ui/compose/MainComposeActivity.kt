8d247b00d5b21da25a9c97b765e0199283cad3c2
Caique Brito <caique.brito.ads@gmail.com>
2023-03-14 23:44:34 -0300

    PARTIAL added destinations

--- presentation/src/main/java/com/marvelcomics/brito/presentation/main/ui/compose/MainComposeActivity.kt
+++ presentation/src/main/java/com/marvelcomics/brito/presentation/main/ui/compose/MainComposeActivity.kt
@@ -1,11 +1,20 @@
 package com.marvelcomics.brito.presentation.main.ui.compose
 
 import android.os.Bundle
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.Navigation
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.marvelcomics.brito.presentation.NavGraphs
 import com.marvelcomics.brito.presentation.R
 import com.marvelcomics.brito.presentation.databinding.ActivityMainComposeBinding
+import com.marvelcomics.brito.presentation.ui.compose.theme.MarvelComicsAppTheme
 import com.marvelcomics.brito.presentation.ui.extensions.viewBinding
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 
 class MainComposeActivity : AppCompatActivity() {
 
@@ -14,11 +23,11 @@ class MainComposeActivity : AppCompatActivity() {
         Navigation.findNavController(this, R.id.nav_host_fragment_compose)
     }
 
+    @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialNavigationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        /*
-        * setContent {
+//        setContentView(binding.root)
+        setContent {
             MarvelComicsAppTheme {
                 val navController = rememberAnimatedNavController()
                 BackHandler {
@@ -27,10 +36,9 @@ class MainComposeActivity : AppCompatActivity() {
                 DestinationsNavHost(
                     navGraph = NavGraphs.root,
                     engine = rememberAnimatedNavHostEngine(),
-                    navController = navController,
+                    navController = navController
                 )
             }
         }
-        * */
     }
 }
